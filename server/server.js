f617e153c6c06057a88726af0c5009c96beec671
James Cramton <jcramton@220OFFICE.local>
2017-09-11 16:22:52 -0700

    add GET /totods/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -28,6 +30,31 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+// GET /todos/123456
+app.get('/todos/:id', (req, res) => {
+  // get id from url
+  var id = req.params.id
+
+  // validate id is legit using isValid. give 404 wsith empty body-parser
+  if(!ObjectID.isValid(id)) {
+    res.status(404).send();
+    return console.log('ID not valid, yo.');
+  }
+  Todo.findById(id).then((todo) => {
+    // if no todo send 404 with emtpy body
+    if(!todo) {
+      res.status(404).send();
+      return console.log ('ID not found');
+    }
+    // if todo send back todo
+    res.send({todo});
+  }, (e) => {
+    res.status(400).send();
+  });
+
+});
+
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
