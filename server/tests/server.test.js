d53d58140ee9ca9a467bc98c8f14443941c7a288
James Cramton <jcramton@220OFFICE.local>
2017-10-13 12:00:59 -0700

    test cases for user creation and validation

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,23 +3,12 @@ const request = require('supertest');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {User} = require('./../models/user');
 const {ObjectID} = require('mongodb');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
-const todos = [{
-  _id: new ObjectID(),
-  text: 'Foist test todo'
-}, {
-  _id: new ObjectID(),
-  text: 'Second one, yo.',
-  completed: true,
-  completedAt: 333
-}];
-
-beforeEach((done) => {
-  Todo.remove({}).then(() =>  {
-    return Todo.insertMany(todos);
-  }).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
@@ -159,7 +148,7 @@ describe('PATCH /todos/:id', () => {
       })
       .end(done);
   });
-  it('should clear completedAtwhen todo is not completed', (done) => {
+  it('should clear completedAt when todo is not completed', (done) => {
     // grab the id of second todo item
     var hexId = todos[1]._id.toHexString();
     var newText = "This is a new world order, yo!!";
@@ -178,6 +167,117 @@ describe('PATCH /todos/:id', () => {
         expect(res.body.todo.completedAt).toNotExist;
       })
       .end(done);
+
+  });
+});
+
+describe('GET /user/me', () => {
+  it('should return user if authenticated', (done) => {
+      request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+          expect(res.body._id).toBe(users[0]._id.toHexString());
+          expect(res.body.email).toBe(users[0].email);
+      })
+      .end(done);
+  });
+
+  it('should return 401 if not authenticated', (done) => {
+    request(app)
+    .get('/users/me')
+    .expect(401)
+    .expect((res) => {
+      expect(res.body).toEqual({});
+    })
+    .end(done);
+  });
+
+});
+
+describe('POST /users', () => {
+  it('should create a user and return user', (done) => {
+    var email = 'example@example.com';
+    var password = '123msadf!';
+
+    request(app)
+    .post('/users')
+    .send({email, password})
+    .expect(200)
+    .expect((res) => {
+      expect(res.headers['x-auth']).toExist();
+      expect(res.body._id).toExist();
+      expect(res.body.email).toBe(email);
+    })
+    .end((err) => {
+      if(err) {
+        return done(err);
+      }
+      User.findOne({email}).then((user) => {
+        expect(user).toExist();
+        expect(user.password).toNotBe(password);
+        done();
+      });
+    });
+  });
+
+  it('should return validation errors if password is invalid', (done) => {
+    var emailgood = 'foo@bar.com';
+    var passwordbad = '123';
+
+    request(app)
+      .post('/users')
+      .send({emailgood, passwordbad})
+      .expect(400)
+      .expect((res) => {
+        expect(res.body._message).toEqual('User validation failed');
+      })
+      .end(done)
+  });
+
+  it('should return validation errors if email is invalid', (done) => {
+    var emailbad = 'foo@bar';
+    var passwordgood = '1234156';
+
+    request(app)
+      .post('/users')
+      .send({emailbad, passwordgood})
+      .expect(400)
+      .expect((res) => {
+        expect(res.body._message).toEqual('User validation failed');
+      })
+      .end(done)
+  });
+
+  it('should return validation errors if both email and password are invalid', (done) => {
+    var passwordbad = '123';
+    var emailbad = 'foo@bar';
+
+    request(app)
+      .post('/users')
+      .send({emailbad, passwordbad})
+      .expect(400)
+      .expect((res) => {
+        expect(res.body._message).toEqual('User validation failed');
+      })
+      .end(done)
+  });
+
+  it('should not create user if email in use', (done) => {
+  // email already in use
+  // return 400
+    request(app)
+      .post('/users')
+      .send({
+        email: users[0].email,
+        password: '123456'
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.body.code).toEqual('11000');
+      })
+      .end(done)
 
   });
 });
