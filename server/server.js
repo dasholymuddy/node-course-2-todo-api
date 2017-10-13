6084e22331f196ce20b359a5ab7b19fb664001a2
James Cramton <jcramton@220OFFICE.local>
2017-10-13 13:09:29 -0700

    added tests for POST /users/login

--- server/server.js
+++ server/server.js
@@ -123,7 +123,6 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
-// response.send with email, password
 app.post('/users/login', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
 
